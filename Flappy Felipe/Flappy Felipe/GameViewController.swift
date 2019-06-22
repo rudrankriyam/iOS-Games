d2f5f681c8cb52f50ebe61be446ebbc3a158a0e4
Rudrank <rudrankriyam@gmail.com>
2019-06-22 14:49:51 +0530

    Initial commit for Flappy Felipe

---
+++ Flappy Felipe/Flappy Felipe/GameViewController.swift
@@ -0,0 +1,41 @@
+//
+//  GameViewController.swift
+//  Flappy Felipe
+//
+//  Created by Rudrank Riyam on 22/06/19.
+//  Copyright © 2019 Rudrank. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        if let skView = self.view as? SKView {
+            if skView.scene == nil {
+                let aspectRatio = skView.bounds.size.height / skView.bounds.size.width
+                let scene = GameScene(size: CGSize(width: 320, height: 320 * aspectRatio))
+            }
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
